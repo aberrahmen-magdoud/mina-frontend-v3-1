35e8edc97806cece7970727cc4d9101f1c6b8c34
moutaamadani <madanimoutaa.visions@gmail.com>
2025-12-13 13:27:21 +0400

    Update MinaApp.tsx

--- src/MinaApp.tsx
+++ src/MinaApp.tsx
@@ -2,7 +2,7 @@
 // ============================================================================
 // [PART 1 START] Imports & environment
 // ============================================================================
-import React, { useEffect, useState, useRef  from "react";
+import React, { useEffect, useState, useRef}  from "react";
 import { supabase } from "./lib/supabaseClient";
 import StudioLeft from "./StudioLeft";
 
