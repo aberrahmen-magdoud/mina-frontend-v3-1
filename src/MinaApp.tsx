ebde3d9936f2af68853daee203504ec2e49c8b62
moutaamadani <madanimoutaa.visions@gmail.com>
2025-12-14 20:20:54 +0400

    Update MinaApp.tsx

--- src/MinaApp.tsx
+++ src/MinaApp.tsx
@@ -1393,7 +1393,7 @@ const MinaApp: React.FC<MinaAppProps> = ({ initialCustomerId }) => {
   if (logoUrl && isHttpUrl(logoUrl)) {
     payload.logoImageUrl = logoUrl;
   }
-rationUrls = uploads.inspiration
+insprationUrls = uploads.inspiration
         .map((u) => u.remoteUrl || u.url)
         .filter((u) => isHttpUrl(u))
         .slice(0, 4);
